16340afb8d3e247c4d887bafa87df983fa28df02
Michał Mrzygłód <mmrzyglod1@gmail.com>
2023-03-29 13:11:40 +0200

    First iteration

---
+++ src/Clients/RawClient/Brokers/Broker.cs
@@ -0,0 +1,19 @@
+﻿namespace RawClient.Brokers;
+
+public class Broker
+{
+    public Broker(Guid brokerId, string url, int port, string login, string password)
+    {
+        Id = brokerId;
+        Port = port;
+        Url = url;
+        Login = login;
+        Password = password;
+    }
+
+    public Guid Id { get; private set; }
+    public int Port { get; set; }
+    public string Url { get; set; }
+    public string Login { get; set; }
+    public string Password { get; set; }
+}
